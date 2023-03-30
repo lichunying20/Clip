c3f053a80b33fe3bd76dc0e468f6e8509bf4b53b
lichunying20 <128216499+lichunying20@users.noreply.github.com>
2023-03-30 19:44:16 +0800

    Add files via upload

---
+++ data augmentation.py
@@ -0,0 +1,21 @@
+import torch
+import torchvision. transforms as transforms
+from PIL import Image
+
+# 定义数据增广函数
+transform = transforms . Compose([
+    transforms.RandomResizedCrop(256, scale=(0.5, 0.5), ratio=(1.0, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=(-10, 10)),
+    transforms . ToTensor(),
+])
+# 加载图片
+image1 = Image.open(r'D:\GitHub_test\classification-basic-sample\data\train\train\cat.jpg')
+
+# 对图片进行增广
+augmented_image1 = transform(image1)
+
+# 显示增广后的图片
+import matplotlib.pyplot as plt
+plt . imshow(augmented_image1 . permute(1, 2, 0))
+plt. show()
